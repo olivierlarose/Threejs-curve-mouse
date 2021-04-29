fc39c26a21452d68f254d8c1e2669a14a8f65d2e
Olivier Larose <olivierlarose1@gmail.com>
2021-04-29 19:29:02 -0400

    added shader

--- src/components/curve.js
+++ src/components/curve.js
@@ -9,7 +9,19 @@ export default class Curve extends Component {
     constructor(props){
         super(props);
         this.cube = null;
-        this.mouse = new THREE.Vector3(0, 0, 0)
+        this.mouse = new THREE.Vector3(0, 0, 0);
+        this.curveStrength = 0.2;
+        this.uniforms = {
+            uTexture: {
+              value: null
+            },
+            uOffset: {
+              value: new THREE.Vector2(0.0, 0.0)
+            },
+            uAlpha: {
+              value: 1
+            }
+        }
     }
 
     componentDidMount(){
@@ -17,14 +29,47 @@ export default class Curve extends Component {
         this.camera = new THREE.PerspectiveCamera(55, window.innerWidth/window.innerHeight, 1, 5);
         this.renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
         this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.clock = new THREE.Clock();
         this.mount.appendChild( this.renderer.domElement );
 
-        const geometry = new THREE.PlaneGeometry(658/1000, 817/1000);
+        const geometry = new THREE.PlaneGeometry(658/1000, 817/1000, 10, 30);
         const loader = new THREE.TextureLoader();
         loader.load(process.env.PUBLIC_URL + '/images/car.png', (texture) => {
-            const material = new THREE.MeshBasicMaterial({
-                map: texture
-            })
+            this.uniforms.uTexture.value = texture;
+
+            const material = new THREE.ShaderMaterial({
+                uniforms: this.uniforms,
+                vertexShader: `
+                  uniform vec2 uOffset;
+                  varying vec2 vUv;
+                  #define M_PI 3.1415926535897932384626433832795
+
+                  vec3 deformationCurve(vec3 position, vec2 uv, vec2 offset) {
+                    position.x = position.x + (sin(uv.y * M_PI) * offset.x);
+                    position.y = position.y + (sin(uv.x * M_PI) * offset.y);
+                    return position;
+                  }
+
+                  void main() {
+                    vUv = uv;
+                    vec3 newPosition = deformationCurve(position, uv, uOffset);
+                    gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );
+                  }
+                `,
+                fragmentShader: `
+                  uniform sampler2D uTexture;
+                  uniform float uAlpha;
+                  varying vec2 vUv;
+            
+                  void main() {
+                    vec3 color = texture2D(uTexture,vUv).rgb;
+                    gl_FragColor = vec4(color,1.0);
+                  }
+                `,
+                transparent: true
+              })
+
+            
             this.cube = new THREE.Mesh(geometry, material);
             this.camera.position.z = 2;
             this.scene.add(this.cube);
@@ -47,16 +92,16 @@ export default class Curve extends Component {
     animate() {
         requestAnimationFrame(this.animate.bind(this));
         if(this.cube == null) return;
-        const { x, y } = this.mouse;
-        
-        const offsetY = y - this.cube.position.y;
-        const offsetX = x - this.cube.position.x
 
+        this.uniforms.uOffset.value  = this.cube.position.clone().sub(this.mouse).multiplyScalar(-this.curveStrength);
+
+        const { x, y } = this.mouse;
         TweenLite.to(this.cube.position, 1, {
             x: x,
             y: y,
             ease: Power4.easeOut
         })
+
         this.renderer.render( this.scene, this.camera);
     }
 
